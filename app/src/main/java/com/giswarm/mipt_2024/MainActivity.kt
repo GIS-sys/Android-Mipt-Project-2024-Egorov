fbc72c2775d782ea0566cb63872cdd43a16c0a00
EgorovGA <egorov.ga@minc.tech>
2024-03-09 18:13:35 +0300

    play around with basic template

--- app/src/main/java/com/giswarm/mipt_2024/MainActivity.kt
+++ app/src/main/java/com/giswarm/mipt_2024/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.giswarm.mipt_2024.ui.theme.MIPT_2024Theme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +25,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting("Angelina")
                 }
             }
         }
@@ -31,16 +34,18 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Surface(color = Color.Magenta) {
+        Text(
+            text = "Hello to you, $name!",
+            modifier = modifier.padding(16.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MIPT_2024Theme {
-        Greeting("Android")
+        Greeting("Angelina")
     }
 }
