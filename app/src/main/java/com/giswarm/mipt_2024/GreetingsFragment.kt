7d0b7aa17e781508fe90dbf089a257523b60de95
EgorovGA <egorov.ga@minc.tech>
2024-03-10 03:49:33 +0300

    add inner fragment

--- app/src/main/java/com/giswarm/mipt_2024/GreetingsFragment.kt
+++ app/src/main/java/com/giswarm/mipt_2024/GreetingsFragment.kt
@@ -9,16 +9,13 @@ import androidx.fragment.app.commit
 class GreetingsFragment : Fragment(R.layout.fragment_greetings) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        // Assuming you have a button with id R.id.xxx in your fragment layout
         val button = view.findViewById<Button>(R.id.btn_go_to_main)
 
         button.setOnClickListener {
-            // Replace the current fragment with WorkingFragment
             parentFragmentManager.commit {
                 setReorderingAllowed(true)
                 replace(R.id.fragment_root_container_view, MainFragment())
-                addToBackStack(null) // This allows going back to GreetingsFragment
+                addToBackStack(null)
             }
         }
     }
--- app/src/main/java/com/giswarm/mipt_2024/MainFragment.kt
+++ app/src/main/java/com/giswarm/mipt_2024/MainFragment.kt
@@ -2,10 +2,19 @@ package com.giswarm.mipt_2024
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
 
 class MainFragment : Fragment(R.layout.fragment_main) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.btn_switch_inner_fragment).setOnClickListener {
+            childFragmentManager.commit {
+                setReorderingAllowed(true)
+                replace(R.id.fragment_container_view_main, MainFragment())
+            }
+        }
     }
 }
