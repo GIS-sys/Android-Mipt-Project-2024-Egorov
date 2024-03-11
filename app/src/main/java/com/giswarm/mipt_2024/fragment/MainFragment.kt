8c7783850c3d655cbe0278ea6ae90adc39ca72ca
EgorovGA <egorov.ga@minc.tech>
2024-03-12 02:49:41 +0300

    attempt at restoring data when phone is turned

--- app/src/main/java/com/giswarm/mipt_2024/fragment/MainFragment.kt
+++ app/src/main/java/com/giswarm/mipt_2024/fragment/MainFragment.kt
@@ -1,6 +1,7 @@
 package com.giswarm.mipt_2024.fragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import androidx.fragment.app.Fragment
@@ -8,9 +9,16 @@ import androidx.fragment.app.commit
 import com.giswarm.mipt_2024.R
 
 class MainFragment : Fragment(R.layout.fragment_main) {
+    companion object {
+        private const val KEY_FRAGMENT_INDEX: String = "KEY_FRAGMENT_INDEX"
+    }
     private var fragmentIndex: Long = -1
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        Log.d("DEBUGMAIN", "onViewCreated")
+        if (savedInstanceState != null) {
+            Log.d("DEBUGMAIN", savedInstanceState.getLong(KEY_FRAGMENT_INDEX).toString())
+        }
         cycleInnerFragment()
         view.findViewById<Button>(R.id.main_btn_switch_inner_fragment).setOnClickListener {
             cycleInnerFragment()
@@ -28,6 +36,20 @@ class MainFragment : Fragment(R.layout.fragment_main) {
                 replace(R.id.root_fragment_container_view, CredentialsFragment())
                 addToBackStack(null)
             }
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Log.d("DEBUGMAIN", "onSaveInstanceState")
+        outState.putLong(KEY_FRAGMENT_INDEX, fragmentIndex)
+    }
+
+    override fun onViewStateRestored(savedInstanceState: Bundle?) {
+        super.onViewStateRestored(savedInstanceState)
+        Log.d("DEBUGMAIN", "onViewStateRestored")
+        if (savedInstanceState != null) {
+            Log.d("DEBUGMAIN", savedInstanceState.getLong(KEY_FRAGMENT_INDEX).toString())
         }
     }
 
