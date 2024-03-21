fef835f5bc210f09bed23dd9f931d896ade60a0d
EgorovGA <egorov.ga@minc.tech>
2024-03-21 22:13:49 +0300

    add russian translation

--- app/src/main/java/com/giswarm/mipt_2024/fragment/TextViewFragment.kt
+++ app/src/main/java/com/giswarm/mipt_2024/fragment/TextViewFragment.kt
@@ -27,7 +27,6 @@ class TextViewFragment : Fragment(R.layout.fragment_text_view) {
                 handler.postDelayed(this, 20);
             }
         }
-        handler.post(updater)
     }
 
     override fun onPause() {
