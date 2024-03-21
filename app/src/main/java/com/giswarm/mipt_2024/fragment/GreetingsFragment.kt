359488046b56a2f746e71a0b199b9f4efceeaf91
EgorovGA <egorov.ga@minc.tech>
2024-03-21 16:12:49 +0300

    show grettings only once, add settings

--- app/src/main/java/com/giswarm/mipt_2024/fragment/GreetingsFragment.kt
+++ app/src/main/java/com/giswarm/mipt_2024/fragment/GreetingsFragment.kt
@@ -1,5 +1,6 @@
 package com.giswarm.mipt_2024.fragment
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -12,13 +13,25 @@ import com.giswarm.mipt_2024.position.GpsPositionManager
 import kotlin.concurrent.timer
 
 class GreetingsFragment : Fragment(R.layout.fragment_greetings) {
+    companion object {
+        private const val PREFERENCES_ALREADY_SHOWN = "GREETINGS_ALREADY_SHOWN"
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        if (requireActivity().getSharedPreferences("PREFERENCE_NAME",  Context.MODE_PRIVATE).getBoolean(PREFERENCES_ALREADY_SHOWN, false)) {
+            moveToMain()
+        }
         view.findViewById<Button>(R.id.greetings_btn_go_to_main).setOnClickListener {
-            parentFragmentManager.commit {
-                setReorderingAllowed(true)
-                replace(R.id.root_fragment_container_view, MainFragment())
-            }
+            requireActivity().getSharedPreferences("PREFERENCE_NAME",  Context.MODE_PRIVATE).edit().putBoolean(PREFERENCES_ALREADY_SHOWN, true).commit()
+            moveToMain()
+        }
+    }
+
+    private fun moveToMain() {
+        parentFragmentManager.commit {
+            setReorderingAllowed(true)
+            replace(R.id.root_fragment_container_view, MainFragment())
         }
     }
 }
