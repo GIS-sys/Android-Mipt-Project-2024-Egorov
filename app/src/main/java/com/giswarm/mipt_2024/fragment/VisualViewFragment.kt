000c3da01cbf35da032942d7389d4b7a5f33a08a
EgorovGA <egorov.ga@minc.tech>
2024-03-21 22:57:27 +0300

    attempt at drawing landscape

--- app/src/main/java/com/giswarm/mipt_2024/fragment/VisualViewFragment.kt
+++ app/src/main/java/com/giswarm/mipt_2024/fragment/VisualViewFragment.kt
@@ -5,54 +5,38 @@ import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Rect
 import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.GradientDrawable.Orientation
 import android.graphics.drawable.ShapeDrawable
 import android.graphics.drawable.shapes.OvalShape
 import android.graphics.drawable.shapes.RectShape
 import android.os.Bundle
 import android.view.View
 import android.widget.LinearLayout
+import androidx.appcompat.widget.LinearLayoutCompat.OrientationMode
 import androidx.fragment.app.Fragment
 import com.giswarm.mipt_2024.R
 
 class VisualViewFragment : Fragment(R.layout.fragment_visual_view) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        draw(view)
+
+        draw(view, resources.configuration.orientation == 2)
     }
 
-    private fun draw(view: View) {
-        val bitmap: Bitmap = Bitmap.createBitmap(700, 1000, Bitmap.Config.ARGB_8888)
+    private fun draw(view: View, isLandscape: Boolean) {
+        val left = if (isLandscape) 0 else 100
+        val top = if (isLandscape) 500 else 500
+        val right = if (isLandscape) 600 else 600
+        val bottom = if (isLandscape) 800 else 800
+        val width = if (isLandscape) 1000 else 700
+        val height = if (isLandscape) 700 else 1000
+
+        val bitmap: Bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
         val canvas: Canvas = Canvas(bitmap)
-
-        var shapeDrawable: ShapeDrawable
-
-        // rectangle positions
-        var left = 100
-        var top = 100
-        var right = 600
-        var bottom = 400
-
-        // draw rectangle shape to canvas
-        shapeDrawable = ShapeDrawable(RectShape())
-        shapeDrawable.setBounds( left, top, right, bottom)
-        shapeDrawable.paint.color = Color.parseColor("#009944")
-        shapeDrawable.draw(canvas)
-
-        // oval positions
-        left = 100
-        top = 500
-        right = 600
-        bottom = 800
-
-        // draw oval shape to canvas
-        shapeDrawable = ShapeDrawable(OvalShape())
+        val shapeDrawable = ShapeDrawable(OvalShape())
         shapeDrawable.setBounds( left, top, right, bottom)
         shapeDrawable.paint.color = Color.parseColor("#009191")
         shapeDrawable.draw(canvas)
-
-        // now bitmap holds the updated pixels
-
-        // set bitmap as background to ImageView
-        view.findViewById<LinearLayout>(R.id.visual_view_fragment_layout).background = BitmapDrawable(getResources(), bitmap)
+        view.findViewById<LinearLayout>(R.id.visual_view_fragment_layout).background = BitmapDrawable(resources, bitmap)
     }
 }
