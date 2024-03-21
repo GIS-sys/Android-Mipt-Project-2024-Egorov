dad4eaa9d9a5177cffcec1da3f0e13c91fc855ea
EgorovGA <egorov.ga@minc.tech>
2024-03-21 21:21:22 +0300

    settings

--- app/src/main/java/com/giswarm/mipt_2024/fragment/SettingsFragment.kt
+++ app/src/main/java/com/giswarm/mipt_2024/fragment/SettingsFragment.kt
@@ -2,11 +2,16 @@ package com.giswarm.mipt_2024.fragment
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.giswarm.mipt_2024.R
 
 class SettingsFragment : Fragment(R.layout.fragment_settings) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.settings_btn_save).setOnClickListener {
+            requireActivity().onBackPressed()
+        }
     }
 }
