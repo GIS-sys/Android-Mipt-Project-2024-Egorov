0c0999dd685ea01910630373711a599df0723a2d
EgorovGA <egorov.ga@minc.tech>
2024-04-03 18:59:31 +0300

    delete useless onViewCreated

--- app/src/main/java/com/giswarm/mipt_2024/fragment/CredentialsFragment.kt
+++ app/src/main/java/com/giswarm/mipt_2024/fragment/CredentialsFragment.kt
@@ -6,7 +6,4 @@ import androidx.fragment.app.Fragment
 import com.giswarm.mipt_2024.R
 
 class CredentialsFragment : Fragment(R.layout.fragment_credentials) {
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-    }
 }
